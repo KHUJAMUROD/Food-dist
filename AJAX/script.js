e4695ea377ae7988d017b7f03c00c7bcd5708623
HUNTER <xujanov.xujamurod@gmail.com>
2024-11-21 09:47:49 +0500

    JSON

---
+++ AJAX/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub'),
+    inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', '/current.json');
+    // async, login, pass
+    request.setRequestHeader('Content-type', 'appLication/json; charset=utf8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            // console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Что-то пошло не так";
+        }
+    });
+
+    // status
+    // statusText
+    // response
+    // readyState
+
+
+});
