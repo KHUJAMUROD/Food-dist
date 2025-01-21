ab56de866eb8f5c98b46539dceeb24137e4226f2
HUNTER <xujanov.xujamurod@gmail.com>
2025-01-21 09:39:00 +0500

    modules

---
+++ Webpack/script22.js
@@ -0,0 +1,10 @@
+import * as data from './main22';
+import sayHi from './main22';
+import { default as sayHi } from './main22.js';
+import sayHi from './main22.js';
+
+
+console.log(`${data.one} and ${two}`);
+sayHi();
+
+// data.sayHi();
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import slider from './modules/slider';/*  */
+
+
 window.addEventListener('DOMContentLoaded', function () {
 
-    const tabs = require('./modules/tabs'),
-        modal = require('./modules/modal'),
-        timer = require('./modules/timer'),
-        cards = require('./modules/cards'),
-        forms = require('./modules/forms'),
-        slider = require('./modules/slider')
 
     tabs();
     modal();
