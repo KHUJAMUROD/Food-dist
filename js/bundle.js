c8f2b901b8c8cf89dfbf10e95949a1ab8ec20743
HUNTER <xujanov.xujamurod@gmail.com>
2025-02-01 09:31:04 +0500

    jquery

--- js/bundle.js
+++ js/bundle.js
@@ -1,62 +1,25 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/script.js":
 /*!**********************!*\
   !*** ./js/script.js ***!
   \**********************/
+/***/ (() => {
 
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Unknown option: .presents. Check out https://babeljs.io/docs/en/babel-core/#options for more information about options.\n    at Object.loadPartialConfigAsync (C:\\Users\\User\\Desktop\\food-dist\\node_modules\\@babel\\core\\lib\\config\\index.js:34:85)\n    at Object.loader (C:\\Users\\User\\Desktop\\food-dist\\node_modules\\babel-loader\\lib\\index.js:116:30)\n    at Object.<anonymous> (C:\\Users\\User\\Desktop\\food-dist\\node_modules\\babel-loader\\lib\\index.js:39:12)");
 
-// 'use strict';
+/***/ })
 
-
-// const inputRub = document.querySelector('#rub'),
-//     inputUsd = document.querySelector('#usd');
-
-//     inputRub.addEventListener('input', () => {
-//         const request = new XMLHttpRequest();  
-
-
-//         // request.open(method, url, async, login, pass);
-//         request.open('GET', 'Ajax/current.json');   
-//         request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-//         request.send(); 
-
-
-        
-//         request.addEventListener('load', () => {    
-//             if (request.status === 200) {
-//                 const data = JSON.parse(request.response);
-//                 inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
-//             } else {
-//                 inputUsd.value = 'Что-то пошло не так';
-//             }
-//         });
-
-//     });
-
-
-
-
-
-const inputRub = document.querySelector('#rub'),
-      inputUsd = document.querySelector('#usd');
-
-inputRub.addEventListener('input', () => {
-    const request = new XMLHttpRequest();
-
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-
-    request.addEventListener('load', () => {
-        if (request.status === 200) {
-            const data = JSON.parse(request.response);
-            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
-        } else {
-            inputUsd.value = "Что-то пошло не так";
-        }
-    });
-
-});
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./js/script.js"]();
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
